353eee61c1efa2496083eef38f40ae3c815d3875
Duy Do <duy.ado97@gmail.com>
2018-11-04 01:41:31 -0800

    Fix Spacing

--- insertDataCompanyDb.sql
+++ insertDataCompanyDb.sql
@@ -1,35 +1,35 @@
 INSERT INTO EMPLOYEE
 VALUES ('John','B','Smith','123456789','1965-01-09',
-		'731 Fondren, Houston, TX','M','30000',
+	'731 Fondren, Houston, TX','M','30000',
         '333445555','5');
 INSERT INTO EMPLOYEE
 VALUES ('Franklin','T','Wong','333445555','1955-12-08',
-		'638 Voss, Houston, TX','M','40000',
+	'638 Voss, Houston, TX','M','40000',
         '888665555','5');
 INSERT INTO EMPLOYEE
 VALUES ('Alicia','J','Zelaya','999887777','1968-01-19',
-		'3321 Castle, Spring, TX','F','25000',
+	'3321 Castle, Spring, TX','F','25000',
         '987654321','4');
 INSERT INTO EMPLOYEE
 VALUES ('Jennifer','S','Wallace','987654321','1941-06-20',
-		'291 Berry, Bellaire, TX','F','43000',
+	'291 Berry, Bellaire, TX','F','43000',
         '888665555','4');
 INSERT INTO EMPLOYEE
 VALUES ('Ramesh','K','Narayan','666884444','1962-09-15',
-		'975 Fire Oak, Humble, TX','M','38000',
+	'975 Fire Oak, Humble, TX','M','38000',
         '333445555','5');
 INSERT INTO EMPLOYEE
 VALUES ('Joyce','A','English','453453453','1972-07-31',
-		'5631 Rice, Houston, TX','F','25000',
+	'5631 Rice, Houston, TX','F','25000',
         '333445555','5');
 INSERT INTO EMPLOYEE
 VALUES ('Ahmad','V','Jabbar','987987987','1969-03-29',
-		'980 Dallas, Houston, TX','M','25000',
+	'980 Dallas, Houston, TX','M','25000',
         '987654321','4');
 INSERT INTO EMPLOYEE
 VALUES ('James','E','Borg','888665555','1937-11-10',
-		'450 Stone, Houston, TX','M','55000',
-		'NULL','1');
+	'450 Stone, Houston, TX','M','55000',
+	'NULL','1');
 
 INSERT INTO DEPARTMENT
 VALUES ('Research','5','333445555','1988-05-22');        
