6fe6df9a92052383478664de67f02d3794546d93
DA01171997 <duy.ado97@gmail.com>
2018-11-07 00:06:25 -0800

    Update dbAccessConsole.py

--- dbAccessConsole.py
+++ dbAccessConsole.py
@@ -1,7 +1,6 @@
 #progress:
 #menu is done
-#part a is done
-#part b c are not done
+#query is not done
 
 import mysql.connector
 from mysql.connector import (connection)
@@ -14,6 +13,8 @@ def userAuthentication():
   return userID, pasw
 
 #Connect to database
+#Return false if can't connect
+#return connection if connected
 def establishconnection(uID, pasw):
   try:
     cnx = connection.MySQLConnection(user = uID, password = pasw, host='127.0.0.1', database='company')
@@ -21,10 +22,13 @@ def establishconnection(uID, pasw):
   except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
       print("Something is wrong with your user name or password")
+      return False
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
       print("Database does not exist")
+      return False
     else:
       print(err)
+      return False
   else:
     cnx.close()
 
@@ -40,22 +44,21 @@ def menuPrompt():
 
 #Option A 
 def optionA(cnx):
-  print("Option a")
   cursor = cnx.cursor()
   supervisorLname = input("Please enter supervisor last name: ")
-  RecursiveSupervisorHierarchy(cursor, supervisorLname, None)
+  semiRecursiveSupervisorHierarchy(cursor, supervisorLname, None)
   cursor.close()
 
-def RecursiveSupervisorHierarchy(cursor, supervisorLname, Ssn = None):
-  #Get supervisor last name
-  sLnameUpper = supervisorLname
-  sLnameUpper.upper()
-  #***Base case if provided with Ssn***
+#Recursively print supervisor's supervisees and 
+#any supervisees under his hierarchy 
+def semiRecursiveSupervisorHierarchy(cursor, supervisorLname, Ssn = None):
+ 
+  #***Base case query if provided with Ssn***
   if Ssn is not None:
     query1 = ("SELECT E.Fname, E.Lname, E.Ssn " \
               "FROM EMPLOYEE AS E " \
               "WHERE E.Ssn = '%s'" % Ssn)
-  #***Recursive case if provided with Lname***
+  #***Recursive case query if provided with Lname***
   else:
     query1 = ("SELECT E.Fname, E.Lname, E.Ssn " \
               "FROM EMPLOYEE AS E " \
@@ -63,30 +66,32 @@ def RecursiveSupervisorHierarchy(cursor, supervisorLname, Ssn = None):
   cursor.execute(query1)
   row1s = cursor.fetchall()
   row1sCount = len(row1s)
-
+  
   #Supervisor doesn't exist
   if (row1sCount == 0):
-    print("EMPLOYEE %s DOES NOT EXISTS", sLnameUpper)
-  #Multiple supervisor with same Last name
+    print("EMPLOYEE %s DOES NOT EXISTS" % supervisorLname)
+  
+  #Multiple supervisors with same last name
   elif (row1sCount > 1):
     print("Multiple supervisor with last name", supervisorLname)
     for (Fname, Lname, Ssn) in row1s:
       print( "{}\t{}\t{}\t".format(Fname, Lname, Ssn,))
     selectedSsn = input("Select Ssn from list: ")
-    RecursiveSupervisorHierarchy(cursor, Lname, selectedSsn)  
+    semiRecursiveSupervisorHierarchy(cursor, Lname, selectedSsn)  
+  
   #Fetch supervisee if no 
   #supervisor with same last name
   elif (row1sCount == 1):
-    #print all supervisees
     row2s = queryForAllSupervisees(cursor, supervisorLname, Ssn)
     rows2Count = len(row2s) 
     if rows2Count is 0:
       print(supervisorLname," has no supervisees")
     else:
+      #print all supervisees
       printAllSupervisees(row2s, supervisorLname)
       #print all supervisees's supervisees
       print("")
-      print("\tPRINT %s SUPERVISEES'S SUPERVISEES" % sLnameUpper)
+      print("\t '%s' SUPERVISEES'S SUPERVISEES HIERARCHY" % supervisorLname)
       print("")
       for (_, Lname,Ssn) in row2s:
         rows3 = queryForAllSupervisees(cursor, Lname, Ssn)
@@ -100,48 +105,86 @@ def RecursiveSupervisorHierarchy(cursor, supervisorLname, Ssn = None):
 def queryForAllSupervisees(cursor, supervisorLname, Ssn):
   if Ssn is None:
     query2 = ("SELECT E.Fname, E.Lname, E.Ssn "  \
-                "FROM EMPLOYEE AS E, EMPLOYEE AS S "  \
-                "WHERE E.Super_ssn = S.Ssn AND S.Lname = '%s'" % supervisorLname)
+              "FROM EMPLOYEE AS E, EMPLOYEE AS S "  \
+              "WHERE E.Super_ssn = S.Ssn AND S.Lname = '%s'" % supervisorLname)
   else:
     query2 = ("SELECT E.Fname, E.Lname, E.Ssn "  \
-                "FROM EMPLOYEE AS E, EMPLOYEE AS S "  \
-                "WHERE E.Super_ssn = S.Ssn AND S.Lname = '%s' AND S.Ssn = '%s'" % (supervisorLname, Ssn))
+              "FROM EMPLOYEE AS E, EMPLOYEE AS S "  \
+              "WHERE E.Super_ssn = S.Ssn AND S.Lname = '%s' AND S.Ssn = '%s'" % (supervisorLname, Ssn))
   cursor.execute(query2)
   rows = cursor.fetchall()
   return rows
 
 #Print all supervisees Fname Lname Ssn 
 def printAllSupervisees(rows, supervisorLname):
-  sLnameUpper = supervisorLname
-  sLnameUpper.upper()
-  print("\t\t%s'S SUPERVISEES  " % sLnameUpper)
-  print("FNAME\t\tLNAME\t\tSSN")
+  print("\t\t%s'S SUPERVISEES  " % supervisorLname)
+  print("{:10s}\t{}\t\t{} ".format("Fname", "Lname", "Ssn"))
   print("--------------------------------------------------------")
   for (Fname, Lname, Ssn) in rows:
-    print("{}\t\t{}\t\t{} ".format( Fname, Lname, Ssn))
+    print("{:10s}\t{}\t\t{} ".format(Fname, Lname, Ssn))
   print("--------------------------------------------------------")
 
+#Print top five highest paid workers
+def optionB(cnx):
+  cursor = cnx.cursor()
+  query = ("SELECT E.Fname, E.Lname, E.Ssn, E.Salary " \
+            "FROM EMPLOYEE AS E " \
+            "ORDER BY E.Salary DESC")
+  cursor.execute(query)
+  rows = cursor.fetchall()
+  counter = 0
+  print("\t\t FIVE HIGHEST PAID WORKERS")
+  print ("{:10s}{:10s}{:10s}\t{}".format("Fname", "Lname", "Ssn", "Salary"))
+  print("--------------------------------------------------------")
+  for (Fname, Lname, Ssn, Salary) in rows:
+    if(counter>=5):
+      break
+    print ("{:10s}{:10s}{:10s}\t{}".format(Fname, Lname, Ssn, Salary))
+    counter+=1
+  print("--------------------------------------------------------")
+  cursor.close()
 
-
-
+#Print top five most worked workers
+def optionC(cnx):
+  cursor = cnx.cursor()
+  query = ("SELECT E.Fname, E.Lname, E.SSn, W.hours, P.Pname " \
+           "FROM EMPLOYEE AS E, WORKS_ON AS W, PROJECT AS P " \
+           "WHERE E.Ssn = W.Essn AND P.Pnumber = W.Pno "
+           "ORDER BY W.Hours DESC")
+  counter = 0
+  cursor.execute(query)
+  rows = cursor.fetchall()
+  print("\t\t FIVE MOST WORKED WORKERS")
+  print("{:10s}{:10s}{:10s}\t{}\t{}".format("Fname","Lname", "Ssn", "Hours", "Pname"))
+  print("--------------------------------------------------------")
+  for (Fname, Lname, Ssn, Hours, Pname) in rows:
+    if(counter>=5):
+      break
+    print("{:10s}{:10s}{:10s}\t{}\t{}".format(Fname, Lname, Ssn, Hours, Pname))
+    counter+=1
+  print("--------------------------------------------------------")
+  cursor.close()
 
 def main():
+  #Authentication
   uID, pasw = userAuthentication()
   cnx = establishconnection(uID, pasw)
-  quitFlag = False
-  while (not quitFlag):
-    option = menuPrompt()
-    if   (option == 'a'):
-      optionA(cnx)
-    elif (option == 'b'):
-      print("Option b")
-    elif (option == 'c'):
-      print("Option c")
-    elif (option == 'q'):
-      quitFlag = True
-    else:
-      print("Invalid Option: ", option, "Please choose again ")
-  cnx.close()
+  if cnx is not False:
+    quitFlag = False
+    #Menu
+    while (not quitFlag):
+      option = menuPrompt()
+      if (option == 'a'):
+        optionA(cnx)
+      elif (option == 'b'):
+        optionB(cnx)
+      elif (option == 'c'):
+        optionC(cnx)
+      elif (option == 'q'):
+        quitFlag = True
+      else:
+        print("Invalid Option: ", option, "Please choose again ")
+    cnx.close()
 
 #Run main()
 main() 
